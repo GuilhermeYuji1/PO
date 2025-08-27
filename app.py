2b38696d38c24162db0905196aef72c3b752accb
Guilherme Yuji Matsune <24011598@fecap.br>
2025-08-27 11:40:37 -0300

    Versão1

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('pag2')
+def pag2():
+    return render_template('pag2.html')
+
+@app.route('pag3')
+def pag3():
+    return render_template('pag3.html')     
+
+@app.route('pag4')
+def pag4():
+    return render_template('pag4.html')    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
